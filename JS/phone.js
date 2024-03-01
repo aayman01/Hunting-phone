7e1d518c7d932b4d3c143645367d440f39ce0e43
aayman01 <ayman.alfi098@gmail.com>
2024-03-01 12:54:34 +0600

    final done

--- JS/phone.js
+++ JS/phone.js
@@ -1,6 +1,6 @@
 // console.log('connected phone hunting')
 
-const loadPhone = async (searchText = 'samsung', isShowAll) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
